90bed5e01b33b9b6f13e0ffa6e5034b7a855b0ff
benjame <ashakobe@gmail.com>
2022-06-17 10:45:52 +0800

    first commit

---
+++ src/main/java/com/yuzhen/springbootstepbystep/SpringBootStepbystepApplication.java
@@ -0,0 +1,13 @@
+package com.yuzhen.springbootstepbystep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStepbystepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStepbystepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuzhen/springbootstepbystep/SpringBootStepbystepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuzhen.springbootstepbystep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStepbystepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
